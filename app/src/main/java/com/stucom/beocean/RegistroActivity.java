3f93560b3c7f0d16aeb1951c47f6e49482ca0fab
Torvi99 <torvi1969@gmail.com>
2020-02-04 18:53:06 +0100

    proyecto inical

---
+++ app/src/main/java/com/stucom/beocean/RegistroActivity.java
@@ -0,0 +1,15 @@
+package com.stucom.beocean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_activity);
+    }
+
+}
